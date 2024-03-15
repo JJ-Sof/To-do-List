8fb058855357c937dfcb478cb2ac330133cedaae
JJ-Sof <jjose4@student.gsu.edu>
2024-03-15 05:28:46 -0400

    Completed v4 of app

--- src/components/TaskList2.jsx
+++ src/components/TaskList2.jsx
@@ -10,6 +10,8 @@ const TaskList = () => {
 
     const [tasks, setTasks] = useState([]);
     const [newTask, setNewTask] = useState('');
+    const [filter, setFilter] = useState('all');
+    const [showDropDown, setShowDropDown] = useState(false);
 
     const toggleTask = (taskId) => {
         const updatedTasks = tasks.map(task =>
@@ -23,10 +25,11 @@ const TaskList = () => {
         e.preventDefault();
         const text1 = newTask.trim();
         if (text1 != ''){
-        const newlyAddedTask = {
+            const newlyAddedTask = {
                 id: Date.now(),
                 text: text1,
                 completed: false,
+                isEditing: false
             };
             setTasks([...tasks, newlyAddedTask]);
             localStorage.setItem('tasks', JSON.stringify([...tasks, newlyAddedTask]));
@@ -40,14 +43,54 @@ const TaskList = () => {
         localStorage.setItem('tasks', JSON.stringify(updatedTasks));
     }
 
+    const startEditTask = (taskId) => {
+        const updatedTasks = tasks.map(task => 
+            task.id === taskId ? {...task, isEditing: true} : {...task, isEditing: false}
+        );
+        setTasks(updatedTasks);
+    }
+
+    const saveEditTask = (taskId, newText) => {
+        const updatedTasks = tasks.map(task => 
+            task.id === taskId ? {...task, text: newText, isEditing: false} : task
+        );
+        setTasks(updatedTasks);
+        localStorage.setItem('task', JSON.stringify(updatedTasks));
+    }
+
+    const discardEditTask = (taskId) => {
+        const updatedTasks = tasks.map(task => 
+            task.id === taskId ? {...task, isEditing: false} : task
+        );
+        setTasks(updatedTasks);
+        localStorage.setItem('task', JSON.stringify(updatedTasks));
+    }
+
     const clearList = () => {
         setTasks([]);
         localStorage.removeItem('tasks');
     }
 
+    const filterTasks = (selectedFilter) => {
+        setFilter(selectedFilter);
+        setShowDropDown(false);
+    }
+
+    const filteredTasks = tasks.filter(task => {
+        if (filter === 'all'){
+            return true;
+        }
+        else if (filter === 'completed'){
+            return task.completed;
+        }
+        else if (filter === 'incomplete'){
+            return !task.completed;
+        }
+    })
+
     const leftColumnTasks = [];
     const rightColumnTasks = [];
-    tasks.forEach((task, index) => {
+    filteredTasks.forEach((task, index) => {
         if (index % 2 === 0) {
             leftColumnTasks.push(task);
         } else {
@@ -58,24 +101,35 @@ const TaskList = () => {
     return (
         <div className="task-list-container">
             <form onSubmit = {addTask}>
-                <input
-                    type="text"
-                    value={newTask}
-                    onChange= {(e) => setNewTask(e.target.value)} 
-                    placeholder='Add a task'
-                />
-                <button type="submit">Add Task</button>
-                <button className='clear-button' onClick={clearList}>Clear tasks</button>
+                <div className='add-task-container'>
+                    <input
+                        type="text"
+                        value={newTask}
+                        className='task-input'
+                        onChange= {(e) => setNewTask(e.target.value)} 
+                        placeholder='Add a task'
+                    />
+                    <button type="submit">Add Task</button>
+                    <button className='clear-button' onClick={clearList}>Clear tasks</button>
+                    <div className='drop-down'>
+                        <select className = 'drop-down-menu' value={filter} onChange={(e) => filterTasks(e.target.value)}>
+                            <option value="all">All</option>
+                            <option value="completed">Completed</option>
+                            <option value="incomplete">Incomplete</option>
+                        </select>
+                    </div>
+                </div>
+                
             </form>
             <div className="task-columns">
                 <div className="task-column">
                     {leftColumnTasks.map(task => (
-                        <Task key={task.id} task={task} onChange={toggleTask} onDelete={deleteTask} />
+                        <Task key={task.id} task={task} onChange={toggleTask} onDelete={deleteTask} startEdit={startEditTask} saveEdit={saveEditTask} discardEdit={discardEditTask}/>
                     ))}
                 </div>
                 <div className="task-column">
                     {rightColumnTasks.map(task => (
-                        <Task key={task.id} task={task} onChange={toggleTask} onDelete={deleteTask} />
+                        <Task key={task.id} task={task} onChange={toggleTask} onDelete={deleteTask} startEdit={startEditTask} saveEdit={saveEditTask} discardEdit={discardEditTask}/>
                     ))}
                 </div>
             </div>
--- src/components/Tasks2.jsx
+++ src/components/Tasks2.jsx
@@ -1,20 +1,68 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Form from 'react-bootstrap/Form';
 
-const Task = ({ task, onChange, onDelete }) => {
+const Task = ({ task, onChange, onDelete, startEdit, saveEdit, discardEdit}) => {
+
+    const [editText, setEditText] = useState(task.text);
+    const [originalText, setOriginalText] = useState(task.text);
+
+    useEffect(() => {
+        setEditText(task.text); 
+        setOriginalText(task.text);
+    }, [task.text]);
+
     const handleDelete = () => {
         onDelete(task.id);
     };
 
+    const handleEditChange = (e) => {
+        setEditText(e.target.value)
+    }
+    
+    const handleKeyDown = (e) => {
+        if (e.key === 'Enter'){
+            if (editText.trim() === '') {
+                handleDelete();
+            }
+            else {
+                saveEdit(task.id, editText);
+                setOriginalText(editText)
+            }
+        }
+    }
+
+    const handleBlur = () => {
+        setEditText(originalText);
+        discardEdit(task.id);
+    }
+
     return (
         <div className='task-display'>
-            <Form.Check
-                className={`custom-checkbox  ${task.completed ? 'completed-task' : ''}`}
-                type="checkbox"
-                checked={task.completed}
-                onChange={() => onChange(task.id)}
-                label={task.text}
-            />
+            {task.isEditing ? (
+                <input
+                    type="text"
+                    className='task-edit-input'
+                    value={editText}
+                    onChange={handleEditChange}
+                    onKeyDown={handleKeyDown}
+                    onBlur={handleBlur}
+                    autoFocus
+                />
+            ) : (
+                <>
+                    <Form.Check
+                        className={`custom-checkbox  ${task.completed ? 'completed-task' : 'incomplete-task'}`}
+                        type="checkbox"
+                        checked={task.completed}
+                        onChange={() => onChange(task.id)}
+                        label={task.text}
+                    />
+                    {!task.completed && (
+                        <button className='edit-button ml-auto' onClick={() => startEdit(task.id)}>Edit</button>
+                    )}
+                </>
+            )}
+            
             {task.completed && (
                 <button className="delete-btn ml-auto" onClick={handleDelete}>x</button>
             )}
